c6cca31fd79ed292fe3211cdce467705dd11def4
a6uxa4 <a6uxa4@icloud.com>
2023-05-12 23:33:47 +0600

    save

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import ClientOnly from './components/ClientOnly'
+import RentModal from './components/modals/RentModal'
 import LoginModal from './components/modals/LoginModal'
 import RegisterModal from './components/modals/RegisterModal'
+
 import ToasterProvider from './provider/ToasterProvider'
 import Navbar from './components/navbar/Navbar'
 import './globals.css'
@@ -29,6 +31,7 @@ export default async function RootLayout({
 			<body className={font.className}>
 				<ClientOnly>
 					<ToasterProvider />
+					<RentModal />
 					<LoginModal />
 					<RegisterModal />
 					<Navbar currentUser={currentUser} />
